aa154bccbdeb1681412fbcf941b2c651799f94cb
Thorax <thorax@me.com>
2018-02-27 18:31:07 +0300

    Initial Commit

---
+++ ThoraxMath/ThoraxMath.h
@@ -0,0 +1,19 @@
+//
+//  ThoraxMath.h
+//  ThoraxMath
+//
+//  Created by Yaroslav Pasternak on 27.02.2018.
+//  Copyright © 2018 Teambrella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThoraxMath.
+FOUNDATION_EXPORT double ThoraxMathVersionNumber;
+
+//! Project version string for ThoraxMath.
+FOUNDATION_EXPORT const unsigned char ThoraxMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThoraxMath/PublicHeader.h>
+
+
